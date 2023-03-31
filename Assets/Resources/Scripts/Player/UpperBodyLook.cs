3a92b0f89915f75c93ce1a66e6e086de94b3df84
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2023-03-31 17:54:55 +0900

    lineRenderer & 조준점 UI생성

--- Assets/Resources/Scripts/Player/UpperBodyLook.cs
+++ Assets/Resources/Scripts/Player/UpperBodyLook.cs
@@ -12,7 +12,7 @@ public class UpperBodyLook : MonoBehaviour
         mainCam_.transform.SetParent(this.transform);
         Vector3 newCamPos = mainCam_.transform.localPosition;
 
-        newCamPos.x = -0.78f;
+     //   newCamPos.x = -0.78f;
         newCamPos.y = 2.94f;
         newCamPos.z = 0.18f;
         mainCam_.transform.localPosition = newCamPos;
@@ -23,20 +23,20 @@ public class UpperBodyLook : MonoBehaviour
     }
 
     /// <summary>
-    /// 상체 각도 위아래 보려고 변경
+    /// 상체 각도 위아래 보려고 x축 rotate
     /// </summary>
     private void RotateUpperBodyAxisX()
     {
        // Debug.Log(transform.localRotation);
         // 아래로 바라보는 방향
-        if (transform.localRotation.x < -0.31 )
+        if (transform.localRotation.x <= -0.31 )
         {
             Quaternion q = transform.localRotation;
             q.x = -0.31f;
             transform.localRotation = q;
         }
         // 위로 바라보는 방향
-        if (transform.localRotation.x>0.22)
+        if (transform.localRotation.x>=0.22)
         {
             Quaternion q = transform.localRotation;
             q.x = 0.22f;
--- Assets/Resources/Scripts/Wand/MagicSpawner.cs
+++ Assets/Resources/Scripts/Wand/MagicSpawner.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class MagicSpawner : MonoBehaviour
 {
-    //private
-    private LineRenderer lineRenderer;
+    private Ray ray_;
+    Vector3 hitPos_ = Vector3.zero;
     private float rayMaxDistance_ = 3000;
+    private LineRenderer lineRenderer_;
     private void Awake()
     {
-        lineRenderer = GetComponent<LineRenderer>();    
-        lineRenderer.enabled = false;
+        lineRenderer_ = GetComponent<LineRenderer>();
+        lineRenderer_.positionCount = 2;
+
     }
     private void Update()
     {
         RayShot();
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.DrawLine(GetPos(), hitPos_, Color.green, 1f);
+        }
     }
-    public Transform GetMagicSpawnTransform()
+    public Transform GetTransform()
     {
         return transform;
+    }
+    public Vector3 GetPos()
+    {
+        return transform.position;
+    }
+    public Vector3 GetLocalPos()
+    {
+        return transform.localPosition;
     }
 
     /// <summary>
@@ -26,23 +40,29 @@ public class MagicSpawner : MonoBehaviour
     /// </summary>
     private void RayShot()
     {
-        lineRenderer.enabled = true;
-        lineRenderer.SetPosition(0, transform.position);
+        ray_.origin = GetPos();
+        ray_.direction = transform.forward;
+        //Vector3 wPos = transform.localToWorldMatrix * new Vector4(GetLocalPos().x, GetLocalPos().y, GetLocalPos().z, 1f);
+      //  lineRenderer_.SetPosition(0, wPos);
+        lineRenderer_.SetPosition(0, GetPos());
 
         RaycastHit hit;
-        Vector3 hitPos = Vector3.zero;
-        if (Physics.Raycast(GetMagicSpawnTransform().position, GetMagicSpawnTransform().forward, out hit, rayMaxDistance_))
+        hitPos_ = Vector3.zero;
+        if (Physics.Raycast(ray_, out hit, rayMaxDistance_))
         {
             IInteractableObject target = hit.collider.GetComponentInParent<IInteractableObject>();
             if(target != null)
             {
                 Debug.Log(target.GetName());
             }
-            hitPos = hit.point;
+            hitPos_ = hit.point;
+      
         }
         else
         {
-            hitPos = GetMagicSpawnTransform().position + GetMagicSpawnTransform().forward * rayMaxDistance_;
+            hitPos_ = GetPos() + GetTransform().forward * rayMaxDistance_;
         }
+        lineRenderer_.SetPosition(1, hitPos_);
+        lineRenderer_.enabled = true;
     }
 } // end of class
