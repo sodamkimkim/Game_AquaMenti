f82f0f12a212b329ea0c06a66ef06ae5263d7788
magekty <72486918+magekty@users.noreply.github.com>
2023-04-06 14:49:04 +0900

    MainWithMerge0406_mapAdded

---
+++ Assets/Scripts/PlayerUtilities/InventoryManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject inGameUIGo_ = null;
+
+    public bool isInventoryUIOpen_ { get; set; }
+    public bool isInventoryUIClose_ { get; set; }
+    public void OpenInventoryUIGo()
+    {
+
+    }
+    public void CloseInventoryUIGo()
+    {
+
+    }
+} // end of class
