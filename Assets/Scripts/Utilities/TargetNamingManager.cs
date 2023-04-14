433a62d0352109fb13ff12f1fb3993d8996e003f
sodamkimkim <theka265@gmail.com>
2023-04-14 12:10:40 +0900

    recommit

--- Assets/Scripts/Utilities/TargetNamingManager.cs
+++ Assets/Scripts/Utilities/TargetNamingManager.cs
@@ -6,35 +6,61 @@ using UnityEngine;
 public class TargetNamingManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject map1_ = null;
-    [SerializeField]
     private GameObject[] map1WorkSectionsArr_ = null;
-
-    [SerializeField]
-    private GameObject map2_ = null;
     [SerializeField]
     private GameObject[] map2WorkSectionsArr_ = null;
 
-    private MeshPaintTarget[] meshPaintTarget_m1__w1_ = null;
-    private MeshPaintTarget[] meshPaintTarget_m1__w2_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m1_w1_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m1_w2_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m1_w3_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m1_w4_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m1_w5_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m2_w1_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m2_w2_ = null;
+    private MeshPaintTarget[] meshPaintTarget_m2_w3_ = null;
 
     private Dictionary<string, int[]> sameNameDic_ = new Dictionary<string, int[]>();
     private void Awake()
     {
-        meshPaintTarget_m1__w1_ = map1WorkSectionsArr_[0].gameObject.GetComponentsInChildren<MeshPaintTarget>();
-        meshPaintTarget_m1__w2_ = map1WorkSectionsArr_[1].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        Init();
+        // # Map1
+        DoProcess(meshPaintTarget_m1_w1_, 1,1);
+        DoProcess(meshPaintTarget_m1_w2_,1,2);
+        DoProcess(meshPaintTarget_m1_w3_,1,3);
+        DoProcess(meshPaintTarget_m1_w4_, 1, 4);
+        DoProcess(meshPaintTarget_m1_w5_, 1, 5);
 
-        ChangeTargetName1(meshPaintTarget_m1__w1_);
-        CountTargets(meshPaintTarget_m1__w1_);
-        ChangeTargetName2(meshPaintTarget_m1__w1_);
+        // # Map2
+        DoProcess(meshPaintTarget_m2_w1_, 2, 1);
+        DoProcess(meshPaintTarget_m2_w2_, 2, 2);
+        DoProcess(meshPaintTarget_m2_w3_, 2, 3);
     }
-    private void ChangeTargetName1(MeshPaintTarget[] _meshPaintTargetArr)
+    private void Init()
     {
+        meshPaintTarget_m1_w1_ = map1WorkSectionsArr_[0].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m1_w2_ = map1WorkSectionsArr_[1].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m1_w3_ = map1WorkSectionsArr_[2].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m1_w4_ = map1WorkSectionsArr_[3].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m1_w5_ = map1WorkSectionsArr_[4].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+
+        meshPaintTarget_m2_w1_ = map2WorkSectionsArr_[0].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m2_w2_ = map2WorkSectionsArr_[1].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+        meshPaintTarget_m2_w3_ = map2WorkSectionsArr_[2].gameObject.GetComponentsInChildren<MeshPaintTarget>();
+    }
+    private void DoProcess(MeshPaintTarget[] _targetArr, int _mapIdx, int _wsIdx)
+    {
+        ChangeTargetName1(_targetArr, _mapIdx, _wsIdx);
+        CountTargets(_targetArr);
+        ChangeTargetName2(_targetArr);
+    }
+    private void ChangeTargetName1(MeshPaintTarget[] _meshPaintTargetArr, int _mapIdx, int _wsIdx)
+    {
+
         foreach (MeshPaintTarget mpt in _meshPaintTargetArr)
         {
             // Debug.Log(mpt.gameObject.name);
             string prevName = mpt.gameObject.name;
-            mpt.gameObject.name = prevName + "_1_1";
+            mpt.gameObject.name = prevName + "_"+_mapIdx + "_"+_wsIdx;
 
         }
     }
@@ -55,11 +81,6 @@ public class TargetNamingManager : MonoBehaviour
                 }
             }
         }
-        foreach (var item in sameNameDic_)
-        {
-            //  Debug.Log(item);
-
-        }
     }
     private void ChangeTargetName2(MeshPaintTarget[] _meshPaintTargetArr)
     {
@@ -68,10 +89,12 @@ public class TargetNamingManager : MonoBehaviour
             string targetName = item.name;
             if (sameNameDic_.ContainsKey(targetName) && sameNameDic_[targetName][0] > 0)
             {
-
                 item.name = targetName + "_" + sameNameDic_[targetName][1];
                 sameNameDic_[targetName][1]++;
-
+            }
+            else
+            {
+                item.name = targetName + "_" + 1;
             }
         }
     }
