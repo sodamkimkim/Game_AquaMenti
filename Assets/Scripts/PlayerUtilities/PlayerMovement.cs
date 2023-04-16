e24dcd67cbd380711e18a7e390959c29d692405e
magekty <72486918+magekty@users.noreply.github.com>
2023-04-16 20:22:43 +0900

    commit_CC_Added_MoveDone

--- Assets/Scripts/PlayerUtilities/PlayerMovement.cs
+++ Assets/Scripts/PlayerUtilities/PlayerMovement.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class PlayerMovement : MonoBehaviour, IPlayerMovement
 {
-    private Rigidbody rb_;
+    //private Rigidbody rb_;
+    private CharacterController characterController_;
 
-    private float walkSpeed_ =5f;
-    private float jumpPower_ = 5;
-    private float runSpeed_ = 7.5f;
+    [SerializeField] private float walkSpeed_ = 5f;
+    [SerializeField] private float jumpPower_ = 5f;
+    [SerializeField] private float runSpeed_ = 7.5f;
+    private float gravity_ = -9.8f;
+    private float beforePosY_ = 0f;
+
 
     public bool isGround_ { get; set; }
+    private bool isJump_ { get; set; }
+
     public bool isLeftShiftKeyInput_ { get; set; }
 
     private void Awake()
     {
-        rb_ = this.GetComponent<Rigidbody>();
+        //rb_ = this.GetComponent<Rigidbody>();
+        characterController_ = GetComponent<CharacterController>();
         isGround_ = true;
+        isJump_ = false;
         isLeftShiftKeyInput_ = false;
         //  characterController = this.GetComponent<CharacterController>();
     }
@@ -31,19 +40,47 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
     }
     public void Walk(Vector3 _direction)
     {
-        rb_.velocity = (_direction * walkSpeed_);
-       // transform.position = GetPlayerPos() + _direction * walkSpeed_ * Time.deltaTime;
+        if (isJump_)
+        {
+            gravity_ = jumpPower_;
+            if (transform.position.y >= jumpPower_ + beforePosY_)
+            {
+                isJump_ = false;
+            }
+        }
+        else if (!isJump_ || characterController_.isGrounded == false)
+        {
+            gravity_ += -9.8f * Time.deltaTime;
+        }
+        else
+        {
+            gravity_ = 0f;
+        }
+        _direction.y = gravity_;
+        // rb_.velocity = (_direction * walkSpeed_);
+        // transform.position = GetPlayerPos() + _direction * walkSpeed_ * Time.deltaTime;
         if (isLeftShiftKeyInput_) { Run(_direction); }
+        else characterController_.Move(transform.TransformDirection(_direction) * Time.deltaTime * walkSpeed_);
     }
     public void Run(Vector3 _direction)
     {
         //transform.position = GetPlayerPos() + _direction * runSpeed_ * Time.deltaTime;
-        rb_.velocity = (_direction * runSpeed_);
+        //rb_.velocity = (_direction * runSpeed_);
+        characterController_.Move(transform.TransformDirection(_direction) * Time.deltaTime * runSpeed_);
     }
     public void Jump()
     {
-        if (isGround_) rb_.AddForce(Vector3.up * jumpPower_ , ForceMode.Impulse);
-        isGround_ = false;
+        /*        if (isGround_) rb_.AddForce(Vector3.up * jumpPower_ , ForceMode.Impulse);
+                isGround_ = false;*/
+
+        if (characterController_.isGrounded == true)
+        {
+            isJump_ = true;
+            beforePosY_ = transform.position.y;
+        }
+
+
+
     }
     public void StandUp()
     {
