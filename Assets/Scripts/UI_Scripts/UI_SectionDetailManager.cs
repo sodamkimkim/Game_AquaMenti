2457146a61e967e785af4c53a475fadd6956e955
magekty <72486918+magekty@users.noreply.github.com>
2023-04-19 17:32:24 +0900

    Fixing_commit

--- Assets/Scripts/UI_Scripts/UI_SectionDetailManager.cs
+++ Assets/Scripts/UI_Scripts/UI_SectionDetailManager.cs
@@ -10,6 +10,7 @@ public class UI_SectionDetailManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI intermediaryTMPGUI_ = null;
     [SerializeField] private TextMeshProUGUI storyDescriptionTMPGUI_ = null;
+    [SerializeField] private TextMeshProUGUI titleTMPGUI_ = null;
     private List<Dictionary<string, object>> workSectionDataList = null;
 
 
@@ -23,6 +24,7 @@ public class UI_SectionDetailManager : MonoBehaviour
             {
                 intermediaryTMPGUI_.text = $"의뢰인 : {workSectionDics["Intermediary"].ToString()}";
                 storyDescriptionTMPGUI_.text = $"To 작업자 에게..\n\n{workSectionDics["StoryBegin"].ToString()}";
+                titleTMPGUI_.text = $"{workSectionDics["Intermediary"].ToString()}의\n 의뢰 청소 하기";
             }
         }
     }
